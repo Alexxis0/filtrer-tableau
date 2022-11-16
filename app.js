c38939d5ee2a97c81fdcd0c744d131e09b1a10f9
Alexis Morantin <alexis@air-de-alexis.home>
2022-11-16 17:24:09 +0100

    modif JS optimisation funcntion

--- app.js
+++ app.js
@@ -48,66 +48,68 @@ let table = [
         catégorie: "langage",
     },
 ];
-// create element
-function createTable(item) {
+// create element html
+function createTable(tableau) {
+    for (let i = 0; i < tableau.length; i++) {
     const nouvelleLigne = document.createElement("tr");
-    nouvelleLigne.innerHTML = `<td>${item}</td>`;
+    nouvelleLigne.innerHTML = `<td>${tableau[i].values}</td>`;
     document.querySelector("table").appendChild(nouvelleLigne);
+    }
 }
-
-createTable(table[0].values);
-createTable(table[1].values);
-createTable(table[2].values);
-createTable(table[3].values);
-createTable(table[4].values);
-createTable(table[5].values);
-createTable(table[6].values);
-createTable(table[7].values);
-createTable(table[8].values);
-createTable(table[9].values);
-createTable(table[10].values);
-createTable(table[11].values);
+createTable(table)
 
 
 // filter
-let selectAnimaux = table.filter((item) => {
+let filterAnimaux = table.filter((item) => {
     return item.catégorie == "animaux";
 });
-let selectLangage = table.filter((item) => {
+
+
+
+let filterLangage = table.filter((item) => {
     return item.catégorie == "langage";
 });
-let selectNourriture = table.filter((item) => {
+
+
+
+let filterNourriture = table.filter((item) => {
     return item.catégorie == "nourriture";
 });
-let selectAll = table.filter((item) => {
+
+
+
+let filterAll = table.filter((item) => {
     return item.catégorie;
 });
 
+function createFilter (filter) {
+    for (let i = 0; i < filter.length; i++) {
+        console.log(filter[i].values);
+    }
+};
+
+// createFilter(filterLangage);
 
 
 
-
+// selectionner
 function selection() {
     let optionAnimaux = document.querySelector(".select")[0].selected;
     let optionLangage = document.querySelector(".select")[1].selected;
     let optionNourriture = document.querySelector(".select")[2].selected;
     let optionAll = document.querySelector(".select")[3].selected;
-    
-    
-    
-    
+
     if (optionAnimaux == true) {
         console.log("vrai animaux");
-        console.log(selectAnimaux);
-        selectAnimaux
+        console.log(filterAnimaux);
     } else if (optionLangage == true) {
         console.log("vrai langage");
-        console.log(selectLangage);
+        console.log(filterLangage);
     } else if (optionNourriture == true) {
         console.log("vrai nourriture");
-        console.log(selectNourriture);
+        console.log(filterNourriture);
     } else if (optionAll == true) {
         console.log("vrai all");
-        console.log(selectAll);
+        console.log(filterAll);
     }
 }
